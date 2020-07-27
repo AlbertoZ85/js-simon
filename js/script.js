2a9032db3a2e97fe36f0e365ac49cdc22544ec37
Alberto Zordan <zorberto@gmail.com>
2020-07-27 22:49:35 +0200

    Used 'includes' method + setInterval and thought again about the loop...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// var artist = null; //artist vuoto vuol dire false
+// console.log(artist);
+//
+// var artistA = (artist || []);
+//
+// console.log(artistA);
+
 // Un alert espone 5 numeri casuali.
 // Dopo la chiusura (manuale, cioè facendo click su ok) dell'alert, parte un timer di 30 secondi.
 // Dopo i 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
@@ -13,18 +20,27 @@ var arrPc = [],
 // 2. Genero 5 numeri casuali senza ripetizioni e li salvo in un array (uso un ciclo WHILE perché non conosco il numero esatto di cicli da eseguire per via del controllo sulle ripetizioni)
 while (arrPc.length < n) {
     numPc = numRandom(1,100);
-    if (!checkNum(arrPc,numPc)) {
+    if (!arrPc.includes(numPc)) {
         arrPc.push(numPc);
     }
 }
 
-// console.log('Array PC',arrPc); // oscuro questo log perché l'utente non bari!
+// console.log('Array PC',arrPc);
 
 // 3. Visualizzazione del box di alert con i 5 numeri così generati
 alert('Memorizza questi 5 numeri: ' + arrPc);
 
-// 4. Imposto un timeout di 30 secondi (che iniziano a scorrere dopo che l'utente ha premuto OK sull'alert box). Allo scadere del tempo viene eseguita la funzione 'guess'
-setTimeout(guess,30000);
+// 4. Imposto un conto alla rovescia di 30 secondi (che iniziano a scorrere dopo che l'utente ha premuto OK sull'alert box), visibile alla console. Allo scadere del tempo viene eseguita la funzione 'guess'
+var clock = setInterval(conteggio,1000);
+var i = 30;
+
+function conteggio() {
+    console.log(i--);
+    if (i < 0) {
+        clearInterval(clock);
+        guess();
+    }
+}
 
 
 // *** FUNZIONI *** //
@@ -33,38 +49,24 @@ function numRandom(min,max) {
     return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
-// b. Funzione che verifica la presenza di un numero all'interno di un array (anche qui un ciclo WHILE perché mi basta eseguire il ciclo fintantoché il confronto non dà esito positivo)
-function checkNum(arr,num) {
-    var i = 0;
-    while (i < arr.length) {
-        if (num == arr[i]) {
-            return true;
-        }
-        i++;
-    }
-    return false;
-}
-
-// c. Funzione 'guess' che gestisce il gioco
+// b. Funzione 'guess' che gestisce il gioco
 function guess() {
-    while (arrUser.length < n) { // ciclo WHILE per via del check sulle ripetizioni...
+    for (var i = 0; i < n; i++) { // uso un ciclo FOR perché so esattamente che l'array avrà n elementi
 
-        // chiedo all'utente di inserire un numero fino a riempire l'array (n=5)
+        // chiedo all'utente di inserire un numero fino a riempire l'array
         numUser = parseInt(prompt('Prova a indovinare uno dei numeri che hai appena visto'));
 
         // effettuo un controllo sull'input dell'utente, affinché non digiti un numero già inserito
-        while (checkNum(arrUser,numUser)) {
+        while (arrUser.includes(numUser)) {
             numUser = parseInt(prompt('Hai già inserito questo numero, riprova!'));
         }
 
         // se il numero non è già stato precedentemente inserito, salvo l'input dell'utente nel suo array
         arrUser.push(numUser);
-    }
 
-    // Scrivo un ciclo FOR che esegue un controllo sui numeri inseriti dall'utente per verificarne la corrispondenza o meno con i numeri casuali generati dal PC (qui uso il ciclo FOR perché il controllo va effettuato su tutti gli elementi degli array, ovvero n=5). A riscontro positivo salvo l'elemento nell'array dei numeri indovinati
-    for (var i = 0; i < n; i++) {
-        if (checkNum(arrPc,arrUser[i])) {
-            arrGuessed.push(arrUser[i]);
+        // se il numero corrisponde a uno dei numeri casuali del PC, lo salvo nell'array dei numeri indovinati
+        if (arrPc.includes(numUser)) {
+            arrGuessed.push(numUser);
         }
     }
 
